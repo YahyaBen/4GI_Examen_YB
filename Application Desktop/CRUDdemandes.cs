d5d3bec3fc47bccedb2785eaa1899e9824382f63
BENABDALLAH Yahya <yahya.ben.abdallah@gmail.com>
2020-01-13 11:58:00 +0100

    Question 4 : Partie 2

--- Application Desktop/CRUDdemandes.cs
+++ Application Desktop/CRUDdemandes.cs
@@ -36,18 +36,19 @@ namespace Application_Desktop
         }
         public string Count_Demandes()
         {
-            int A = DB.Demandes.Count(x => x.).where;
+            int A = int.Parse(CB_Prof.SelectedItem.ToString());
+            Demande Dem = DB.Demandes.Where(w => w.ID == A).First();
             return A.ToString();
         }
         private void CB_Prof_SelectedIndexChanged(object sender, EventArgs e)
         {
-            int A = int.Parse(CB_Prof.SelectedItem.ToString());
+            int A = int.Parse(CB_Prof.SelectedIndex.ToString());
             Demande Dem = DB.Demandes.Where(x => x.Professeur_id == A).First();
             DateTime X = Dem.Date_Demande.Value;
             DateTime Y = DateTime.Now;
             TimeSpan Z = X - Y;
             Lab_Ancien.Text =Z.ToString()+" ans";
-            Lab_Nbr_Demandes.Text = "";
+            Lab_Nbr_Demandes.Text = Count_Demandes();
             DGV_Demandes.DataSource = DB.Demandes.Where(x => x.Professeur_id == A).Select(p => new { ID = p.Lycee_id, ID_Mat = p.Lycee.Nom}).ToList();
         }
 
@@ -58,17 +59,41 @@ namespace Application_Desktop
 
         private void Btn_Demand_Ajouter_Click(object sender, EventArgs e)
         {
-
+            D.Lycee_id = int.Parse(CB_Lycee.SelectedItem.ToString());
+            D.Professeur_id = int.Parse(CB_Prof.Text);
+            D.Date_Demande = DateTime.Now;
+            DB.Demandes.Add(D);
+            DB.SaveChanges();
         }
 
         private void Btn_Demand_Modifier_Click(object sender, EventArgs e)
         {
-
+            if(int.Parse(Count_Demandes()) < 3) { 
+            DialogResult D = MessageBox.Show("Voulez-vous modifier cette demandes?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (D == DialogResult.Yes)
+            {
+                int ID = int.Parse(DGV_Demandes.CurrentRow.Cells[0].Value.ToString());
+                Demande DD = DB.Demandes.Where(x => x.ID == ID).First();
+                DD.Lycee_id = int.Parse(CB_Lycee.SelectedItem.ToString());
+                    DD.Professeur_id = int.Parse(CB_Prof.Text);
+                    DD.Date_Demande = DateTime.Now;
+                    DB.SaveChanges();
+                MessageBox.Show("Demmandes Modifier !", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            }
         }
 
         private void Btn_Demand_Supprimer_Click(object sender, EventArgs e)
         {
-
+            DialogResult D = MessageBox.Show("Voulez-vous Supprimer cette demandes?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (D == DialogResult.Yes)
+            {
+                int ID = int.Parse(DGV_Demandes.CurrentRow.Cells[0].Value.ToString());
+                Lycee Lyc = DB.Lycees.Where(x => x.ID == ID).First();
+                DB.Lycees.Remove(Lyc);
+                DB.SaveChanges();
+                MessageBox.Show("Demandes Supprimer !", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
         private void Btn_Demand_Fermer_Click(object sender, EventArgs e)
